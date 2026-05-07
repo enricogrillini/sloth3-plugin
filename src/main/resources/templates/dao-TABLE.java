48ac5e6929debfcd68332532d08b3397047154e4
Enrico Grillini <enrico.grillini@gmail.com>
2026-05-07 08:31:37 +0200

    FIX template pojo

--- src/main/resources/templates/dao-TABLE.java
+++ src/main/resources/templates/dao-TABLE.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ${pojoPackageName}.${pojoClassName};
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
